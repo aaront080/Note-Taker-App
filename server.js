7d7ecc19eecba18d5ffc3e351b5b456b8d18a4b5
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-26 21:31:07 -0700

    api routes added

--- server.js
+++ server.js
@@ -29,5 +29,51 @@ app.get('*', (req,res) => {
 
 //route to return to read and return db.json files
 app.get('/api/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '../'))
-})
+    fs.readFile('./db/db.json', 'uft8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+        }
+        res.json(parsedNotes)
+    });
+});
+
+
+
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}` ));
+
+
+
+
+
+
+
+fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      // return res.status(500).json('Error in posting review');
+    } else {
+      // Convert string into JSON object
+      const parsedReviews = JSON.parse(data);
+
+      // Add a new review
+      parsedReviews.push(newReview);
+
+      currentReviews.push(newReview)
+
+      // Write updated reviews back to the file
+      fs.writeFile(
+        './db/reviews.json',
+        JSON.stringify(parsedReviews, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated reviews!')
+      );
+    }
+  });
+
+
